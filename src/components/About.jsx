2c6119062776e358c1c5c8c6f03b2374b46bf810
Fibi Youssef <fibi057057@gmail.com>
2023-08-27 14:29:51 -0500

    adding to about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,19 +14,35 @@ const About = () => {
         </div>
 
         <p className="text-xl mt-20">
-          I am an aspiring Front-Developer with a background in UX/UI Design.
-          I've designed multiple wireframes and digital prototypes using Figma
-          and Adobe XD. Once I was able to design websites, I wanted to build
-          them from scartch with my own personal touch. I attended and graduated
-          from Nucamp's Full Stack Web and Mobile Development Bootcamp and
-          started using codes such as Bootstrap, React, React Native, Node.js,
-          and many more to start creating my own websites. Come and follow my
-          journey!
+          I'm Fibi Youssef, a passionate UX/UI designer with a knack for
+          creating seamless digital experiences. My journey in the world of
+          design began with building a foundation in UX/UI. I'm well-versed in
+          utilizing tools like Figma and Adobe XD to bring my creative visions
+          to life, ensuring that each pixel serves a purpose. I continued to
+          expand my knowledge by pursuing education in full-stack web
+          development, giving me a unique perspective on the intersection of
+          functionality and aesthetics. With an eye for detail and a
+          user-centric approach, I strive to craft designs that not only look
+          visually appealing but also resonate with users on a meaningful and
+          functional level.
         </p>
 
         <br />
 
-        <p className="text-xl">idk yet</p>
+        <p className="text-xl">
+          Below, you will find some of my front-end development and UX/UI design
+          projects that showcase the culmination of my creative vision and
+          technical skills. Each project encapsulates a unique blend of
+          aesthetics and functionality, highlighting my ability to create
+          visually appealing interfaces that seamlessly integrate with robust
+          and efficient code. From responsive websites that adapt flawlessly to
+          various devices and screen sizes, to intuitive user interfaces that
+          prioritize user interactions, these projects embody my commitment to
+          delivering digital experiences that leave a lasting impact. Feel free
+          to explore my portfolio, where each project is a chapter in my ongoing
+          story of combining code and design to bring meaningful experiences to
+          life.
+        </p>
       </div>
     </div>
   );
