ca5fcbacec8ceda3e56fcf21d31abf669e4266a3
Fibi Youssef <fibi057057@gmail.com>
2023-08-29 16:27:12 -0500

    fixing about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,14 +7,14 @@ const About = () => {
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
+        <div className="pb-4">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
         </div>
 
-        <p className="text-xl mt-20">
-          I'm Fibi Youssef, a passionate UX/UI designer with a knack for
+        <p className="text-xl mt-10">
+          Hi! I'm Fibi Youssef, a passionate UX/UI designer with a knack for
           creating seamless digital experiences. My journey in the world of
           design began with building a foundation in UX/UI. I'm well-versed in
           utilizing tools like Figma and Adobe XD to bring my creative visions
