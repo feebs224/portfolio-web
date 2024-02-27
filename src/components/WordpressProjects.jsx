d1525915febd420748eceef71ddae46fb55cc458
feebs224 <fibi057057@gmail.com>
2024-02-27 10:44:32 -0600

    update

--- src/components/WordpressProjects.jsx
+++ src/components/WordpressProjects.jsx
@@ -66,7 +66,7 @@ const WordpressProjects = () => {
               <div className="flex items-center justify-center">
                 <a href={url1}>
                   <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                    Prototype
+                    Link
                   </button>
                 </a>
               </div>
